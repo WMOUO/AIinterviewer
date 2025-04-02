9ec2315b626ba28cee02b622f803f2665c322310
tallo <liu934800@gmail.com>
2025-04-02 22:32:57 +0800

    使用者畫面.測試

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  vueIndentScriptAndStyle: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always",
+  proseWrap: "preserve",
+}
+
+module.exports = config
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <n-button type="info">
+      Info
+    </n-button>
+  </div>
+</template>
+
+<script lang="js" setup>
+</script>
